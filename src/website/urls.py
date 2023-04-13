e9a0f8c5e5bb99066ce7a8ac7be20f6a4f7ded7a
saqib-devops <saqibahmad778866@gmail.com>
2023-04-13 13:58:34 -0400

    Website:TemplatesView Added for >> comic , novel,blog,contact us , cart ,login , signup , forget password, aboutus, blog detail

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from src.website.views import HomeTemplateView,ShopTemplateView
+from src.website.views import HomeTemplateView, ComicsTemplateView, NovelTemplateView, \
+    BlogDetailTemplateView, ContactUsTemplateView, BlogTemplateView, CartTemplateView, AboutUsTemplateView
 
 app_name = "website"
 urlpatterns = [
-    path('',HomeTemplateView.as_view(),name='home'),
-    path('store/',ShopTemplateView.as_view(),name='store')
+    path('', HomeTemplateView.as_view(), name='home'),
+    path('comics/', ComicsTemplateView.as_view(), name='comic'),
+    path('novels/', NovelTemplateView.as_view(), name='novel'),
+
+    path('stories/', BlogTemplateView.as_view(), name='stories'),
+    path('story-detail/', BlogDetailTemplateView.as_view(), name='stories-detail'),
+
+    path('about-us/', AboutUsTemplateView.as_view(), name='about_us'),
+    path('contact-us/', ContactUsTemplateView.as_view(), name='contact_us'),
+
+    path('cart/', CartTemplateView.as_view(), name='cart'),
 ]
