10a1bcba4c09750ab5e2db9bad549dd6a53c65b8
saqib-devops <saqibahmad778866@gmail.com>
2023-04-19 13:56:02 -0400

    Feat : wishlist list and delete view added

--- src/administration/client/urls.py
+++ src/administration/client/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from src.administration.client.views import ClientDashboard
+from src.administration.client.views import ClientDashboard,WishlistView,WishListDelete
 
 app_name = 'client'
 urlpatterns = [
-    path('', ClientDashboard.as_view(), name='dashboard')
+    path('', ClientDashboard.as_view(), name='dashboard'),
+    path('wishlist/', WishlistView.as_view(), name='wishlist'),
+    path('wishlist/delete/<str:pk>', WishListDelete.as_view(), name='wishlist-delete'),
 ]
--- src/administration/client/views.py
+++ src/administration/client/views.py
@@ -1,9 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import TemplateView, UpdateView, ListView
 
 from src.accounts.models import Address
+from src.administration.admins.models import Wishlist, Order
 
 
 # Create your views here.
@@ -12,6 +15,25 @@ from src.accounts.models import Address
 class ClientDashboard(TemplateView):
     template_name = 'client/client_dashboard.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ClientDashboard, self).get_context_data(**kwargs)
+        context['total_orders'] = Order.objects.filter(user=self.request.user).count()
+        context['pending_orders'] = Order.objects.filter(user=self.request.user, order_status="pending").count()
+        context['wishlist'] = Wishlist.objects.filter(user=self.request.user).count()
+        return context
 
+
+@method_decorator(login_required, name='dispatch')
 class WishlistView(ListView):
-    pass
+    model = Wishlist
+    template_name = 'client/wishlist_list.html'
+    context_object_name = 'objects'
+
+
+@method_decorator(login_required, name='dispatch')
+class WishListDelete(View):
+    def get(self, request, pk, *args, **kwargs):
+        wishlist = get_object_or_404(Wishlist, product_id=pk, user=self.request.user)
+        wishlist.delete()
+        messages.success(request, 'Wishlist Item Deleted Success Fully')
+        return redirect("client:wishlist")
